5ebdc10d03a66d0da593dd3de92e314f90be9f4c
shadee22 <79150173+shadee22@users.noreply.github.com>
2022-08-03 20:15:08 +0530

    Update settings.py

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -79,7 +79,10 @@ WSGI_APPLICATION = 'real_estate.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-   
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    }
 }
 
 
