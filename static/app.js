cb09de18f10de826098e5c21918055f5d544dbdb
shadeer <shadeersadikeen@gmail.com>
2022-08-31 20:11:35 +0530

    create local and live url fixed context error

--- static/app.js
+++ static/app.js
@@ -37,46 +37,41 @@ function init() {
         
             
         var CSRFtoken = $('input[name=csrfmiddlewaretoken]').val();
-        var url = 'http://13.234.226.190:80/image/'
+        var live_url = 'http://13.234.226.190:80/image/'
+        var local_url = 'http://127.0.0.1:8000/image/'
         $(".progress").fadeIn()
-        $.post(url , { 
+        $.post(live_url , { 
             image_data: imageData,
             csrfmiddlewaretoken: CSRFtoken,
-            timeout: 188000 //
+            timeout: 188000 
         },function(data , status){
-            console.log(data)
-            // console.log('completed')
-            // if (data.image_data.length == 0) {
-            //     $(".progress").fadeOut();
-            //     $("#error").fadeIn().delay(5000).fadeOut();
-            //     $("#result").hide(300);
-            //     return;
-            // }
-            // let gdata = data.image_data[0]
+            if (data.image_data.length == 0) {
+                $(".progress").fadeOut();
+                $("#error").fadeIn().delay(5000).fadeOut();
+                $("#result").hide(300);
+                return;
+            }
+            let gdata = data.image_data[0]
             
-            // probs = gdata['probability']
-            // name2number = gdata['dictionary']
-            // function upperCaseString(str){
-            //     str = String(str).charAt(0).toUpperCase() + String(str).slice(1)
-            //     str = str.split('_').join(' ')
-            //     return str
-
-
-            // }
+            probs = gdata['probability']
+            name2number = gdata['dictionary']
+            function upperCaseString(str){
+                str = String(str).charAt(0).toUpperCase() + String(str).slice(1)
+                str = str.split('_').join(' ')
+                return str
+            }
             
-            // console.log(data.image_data.image_data);
-            
-            // jQuery.each(probs, function(i, v) {
-            //     console.log(i)
-            //     n = i + 1
-            //     e = document.createElement("p")
-            //     e.innerHTML = upperCaseString( name2number[i]) + " : " +"<b>" + Math.round(v) + "%"+"</b>";
-            //     $("ul").append(e)
-            // });
-            // $('#result #pred').text(upperCaseString(gdata.prediction));
+            jQuery.each(probs, function(i, v) {
+                n = i + 1
+                e = document.createElement("p")
+                e.innerHTML = upperCaseString( name2number[i]) + " : " +"<b>" + Math.round(v) + "%"+"</b>";
+                $("ul").append(e)
+            });
+            $('#result #pred').text(upperCaseString(gdata.prediction));
             $('#result').show(500)
             $(".progress").fadeOut()
             $('#result-img').attr('src' , '/static/images/har.jpg');
+            console.log('Everything Loaded Properly!')
         })
        
         // dz.removeFile(file)
